e93156909f43223bf659a8ef0efe2f8e1b5cf829
DedLad <adheshathrey2004.com>
2024-08-10 21:31:14 +0530

    feat : added endpoint basic functionality

--- main.go
+++ main.go
@@ -2,17 +2,35 @@
 package main
 
 import (
+	"io"
 	"log"
+	"mime/multipart"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/homebrew-ec-foss/event-loop-backend/handlers"
 )
 
 func main() {
-
 	r := gin.Default()
 
-	r.POST("/create", handlers.HandleCreate)
+	r.POST("/create", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = saveFile(file)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		handlers.HandleCreate(c)
+
+		c.JSON(200, gin.H{"message": "File uploaded successfully"})
+	})
 
 	r.PUT("/checkpoint", handlers.HandleCheckpoint)
 
@@ -22,5 +40,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
+func saveFile(file *multipart.FileHeader) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(file.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
