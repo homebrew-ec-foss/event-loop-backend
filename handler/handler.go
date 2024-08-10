8f09efaa030fc35c9f8c7d111e1e93e4fcff49a8
DedLad <adheshathrey2004.com>
2024-08-10 19:13:53 +0530

    Refactor : New structure

---
+++ handler/handler.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func PutEndpoint(c *gin.Context) {
+}
+
+func GetEndpoint(c *gin.Context) {
+
+}
+func SetEndpoint(c *gin.Context) {
+}
