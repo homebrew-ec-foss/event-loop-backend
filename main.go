f47a1f028a52aef5403fe195269ca0e024a24969
DedLad <adheshathrey2004.com>
2024-08-10 19:09:34 +0530

    Refactor : New structure

--- main.go
+++ main.go
@@ -1,37 +1,24 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gin-gonic/gin"
-	"github.com/homebrew-ec-foss/event-loop-backend/database"
-	"github.com/homebrew-ec-foss/event-loop-backend/vitals"
 )
 
 func main() {
-	router := gin.Default()
+	r := gin.Default()
 
-	// connect to database
-	err := database.Connect("event-loop.db")
-	// we can load the database file name
-	// from a config file in the future.
+	r.PUT("/create/:value", putEndpoint)
+	r.GET("/get/:value", getEndpoint)
+	r.SET("/set/:value", setEndpoint)
 
-	if err != nil {
-		log.Fatal("Failed to connect to database: ", err)
-	} else {
-		log.Println("Connected to database.")
-	}
+	r.Run(":8080")
+}
 
-	router.GET("/ping", vitals.Ping)
+func putEndpoint(c *gin.Context) {
+}
 
-	s := &http.Server{
-		Addr:         ":6969",
-		Handler:      router,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-	log.Println("Listening on port 6969.")
-	s.ListenAndServe()
+func getEndpoint(c *gin.Context) {
+
+}
+func setEndpoint(c *gin.Context) {
 }
