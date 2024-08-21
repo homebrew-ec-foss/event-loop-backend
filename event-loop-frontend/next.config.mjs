cd6713adc06afdbbe520593f7ac6ec10b2c701a8
bwaklog <aditya.mh@outlook.com>
2024-08-21 10:57:57 +0530

    chore: compose.yml added

--- event-loop-frontend/next.config.mjs
+++ event-loop-frontend/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    // GO_BACKEND_URL: "https://localhost:8080",
+    env: {
+        // NOTE:
+        // GO_BACKEND_URL: "replace with ip of VM"
 
-    // Hegde's Tailscale IP to backend
-    // GO_BACKEND_URL: "https://100.91.203.91:8080",
+        // GO_BACKEND_URL: "https://0.0.0.0:8080",
 
-    // Sudhir's Tailscale IP to backend
-    // GO_BACKEND_URL: "https://100.102.173.22:8080",
-  },
+        // Hegde's Tailscale IP to backend
+        // GO_BACKEND_URL: "https://100.91.203.91:8080",
+
+        // Sudhir's Tailscale IP to backend
+        // GO_BACKEND_URL: "https://100.102.173.22:8080",
+    },
 };
 
 export default nextConfig;
